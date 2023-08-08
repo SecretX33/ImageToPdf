ff90eead0bf696e7e78a7b738b1689efc3bb19d3
SecretX33 <4389757+SecretX33@users.noreply.github.com>
2023-08-07 21:58:18 -0300

    Add 3 dev scripts to facilitate native image testing

---
+++ scripts/dev/native_dev_run.bat
@@ -0,0 +1,18 @@
+@echo off
+SETLOCAL EnableExtensions
+
+set "EXE_PATH=build\native\nativeCompile\ImageToPdf.exe"
+
+cd ..
+cd ..
+
+if not exist "%EXE_PATH%" (
+    echo Could not find application compiled binary, please make sure the app is compiled before running this script
+    pause>nul
+    goto END
+)
+
+.\%EXE_PATH% --jpg-quality 0.85 --resize 0.65 --sort modified_date "D:\Local Disk\Users\User\Pictures\halion\halion de frente.png" "D:\Local Disk\Users\User\Pictures\halion\segredos de tank.png"
+
+:END
+endlocal
