86c1db18c3eeea047838b4f7c579d2911e1a053e
SecretX <4389757+SecretX33@users.noreply.github.com>
2023-10-16 00:02:46 -0300

    Feat: Improve image processing speed (#3)

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
-    kotlin("jvm") version "1.9.0"
-    kotlin("kapt") version "1.9.0"
-    id("org.graalvm.buildtools.native") version "0.9.23"
+    kotlin("jvm") version "1.9.10"
+    kotlin("kapt") version "1.9.10"
+    id("org.graalvm.buildtools.native") version "0.9.27"
     application
 }
 
 group = "com.github.secretx33"
-version = "0.1.2"
+version = "0.2"
 
 val javaVersion = 17
 
@@ -22,12 +22,12 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.7.3"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
-    implementation("org.apache.pdfbox:pdfbox:3.0.0-beta1")
+    implementation("org.apache.pdfbox:pdfbox:3.0.0")
     implementation("com.1stleg:jnativehook:2.1.0")
-    implementation("info.picocli:picocli:4.7.4")
-    kapt("info.picocli:picocli-codegen:4.7.4")
-    implementation("org.fusesource.jansi:jansi:2.4.0")
-    implementation(platform("io.arrow-kt:arrow-stack:1.2.0"))
+    implementation("info.picocli:picocli:4.7.5")
+    kapt("info.picocli:picocli-codegen:4.7.5")
+    implementation("org.fusesource.jansi:jansi:2.4.1")
+    implementation(platform("io.arrow-kt:arrow-stack:1.2.1"))
     implementation("io.arrow-kt:arrow-fx-coroutines")
 }
 
--- src/main/kotlin/com/github/secretx33/imagetopdf/model/PdfImage.kt
+++ src/main/kotlin/com/github/secretx33/imagetopdf/model/PdfImage.kt
@@ -1,14 +1,17 @@
 package com.github.secretx33.imagetopdf.model
 
 import org.apache.pdfbox.pdmodel.PDDocument
-import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject
+import java.awt.Dimension
+import java.awt.image.BufferedImage
 import java.nio.file.Path
 
 data class PdfImage(
-    val image: PDImageXObject,
+    val image: BufferedImage,
     val fileName: Path,  // Only the name and extension
     val file: Path,      // The full path to the original image
     val width: Int,
     val height: Int,
     val document: PDDocument,
-)
+) {
+    val dimension = Dimension(width, height)
+}
